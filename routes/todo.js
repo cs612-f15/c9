9540a468b682905d4a5d05aceb5ed592d6573121
Shawn Cicoria <shawn@cicoria.com>
2015-10-16 14:45:57 +0000

    error handling

--- routes/todo.js
+++ routes/todo.js
@@ -7,7 +7,11 @@ var db = require('../db');
 exports.list = function (req, res) {
 	var collection = db.get().collection('todo');
 	// var collection = db.get().collection('todo')
-	collection.find().toArray(function (err, docs) {
+	collection.find().limit(4).sort({'id':-1 }).toArray(function (err, docs) {
+		if (err){
+			res.send({'error' : err});
+			return;
+		}
 		res.send(docs);
 	})
 
